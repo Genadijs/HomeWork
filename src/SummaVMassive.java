86e5876565fd5e3cb4afb094c2363c79d1478f2c
Gennadijs <genadijs.vladimirovs@gmail.com>
2018-07-12 21:51:46 +0300

    HomeWork

---
+++ src/SummaVMassive.java
@@ -0,0 +1,9 @@
+public class SummaVMassive {
+    public static void main (String []args){
+        int[]a = {3,5};
+        int resoult = 0;
+        for (int i=0; i<10; i++){
+            resoult = resoult + a[i];
+        }
+    }
+}
---
+++ src/WaterCan.java
@@ -0,0 +1,2 @@
+public class WaterCan {
+}
