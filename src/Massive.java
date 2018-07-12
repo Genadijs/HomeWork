8f793a903f35578ee2c428673a16ff8a324a57bb
Gennadijs <genadijs.vladimirovs@gmail.com>
2018-07-12 21:57:23 +0300

    HomeWork

---
+++ src/Massive.java
@@ -0,0 +1,9 @@
+public class Massive {
+    public static void main (String[]args){
+        int[] a = new int[10];
+        for (int i = 0; i <10; i++){
+            a[i]=i;
+            System.out.println(a[i]);
+        }
+    }
+}
